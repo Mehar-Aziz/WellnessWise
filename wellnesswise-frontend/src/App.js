0288163922cbe98fa605c064e85ebc4c6f0b88b8
Mehar-Aziz <meharaziz.1614>
2024-07-18 16:13:00 +0500

    Login

--- wellnesswise-frontend/src/App.js
+++ wellnesswise-frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"; 
+import Login from "./components/Login";
 
 function App() {
   return (
     <div id="root">
       <div className="App">
         <Navbar />
-        {/* Your main content here */}
+        <div className="content">
+          {/* Routing logic or conditional rendering based on state */}
+          <Login />
+          {/* <Registration /> */}
+        </div>
+      
       </div>
       <Footer />
     </div>
---
+++ wellnesswise-frontend/src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './Login.css'
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Mock login validation (replace with your own logic)
+    if (email === "test@example.com" && password === "password123") {
+        // Mock successful login action
+        console.log("Login successful!");
+        // Reset form fields
+        setEmail("");
+        setPassword("");
+      } else {
+        // Mock unsuccessful login action
+        console.log("Error")
+      }
+    };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
