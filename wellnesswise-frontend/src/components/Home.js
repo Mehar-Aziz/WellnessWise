ffb6ca354679862de04c031fbe6d66a04e9840ae
Mehar-Aziz <meharaziz.1614>
2024-07-19 19:54:13 +0500

    Adjust Contact us

--- wellnesswise-frontend/src/components/Home.js
+++ wellnesswise-frontend/src/components/Home.js
@@ -84,33 +84,33 @@ function Home() {
             </section>
 
             {/* Contact Us */}
-            <section id="contact_us" className="contact-us-single">
+            <section id="contact_us" className="contact-us-single for-gap">
                 <div className="row no-margin">
 
                     <div className="col-sm-6 cop-ck">
-                        <h2>CONTACT</h2>
+                        <h2>CONTACT US</h2>
                         <div className="row cf-ro">
                             <div className="col-sm-3"><label>Enter Name:</label></div>
-                            <div className="col-sm-8"><input type="text" placeholder="Enter Name" name="name" className="form-control input-sm" /></div>
+                            <div className="col-sm-8"><input type="text"  name="name" className="form-control input-sm" /></div>
                         </div>
                         <div className="row cf-ro">
                             <div className="col-sm-3"><label>Email Address:</label></div>
-                            <div className="col-sm-8"><input type="text" name="email" placeholder="Enter Email Address" className="form-control input-sm" /></div>
+                            <div className="col-sm-8"><input type="text" name="email"  className="form-control input-sm" /></div>
                         </div>
                         <div className="row cf-ro">
                             <div className="col-sm-3"><label>Mobile Number:</label></div>
-                            <div className="col-sm-8"><input type="text" name="mobile" placeholder="Enter Mobile Number" className="form-control input-sm" /></div>
+                            <div className="col-sm-8"><input type="text" name="mobile"  className="form-control input-sm" /></div>
                         </div>
                         <div className="row cf-ro">
                             <div className="col-sm-3"><label>Enter Message:</label></div>
                             <div className="col-sm-8">
-                                <textarea rows="5" placeholder="Enter Your Message" className="form-control input-sm"></textarea>
+                                <textarea rows="5"  className="form-control input-sm"></textarea>
                             </div>
                         </div>
                         <div className="row cf-ro">
                             <div className="col-sm-3"><label></label></div>
                             <div className="col-sm-8">
-                                <button className="btn btn-success btn-sm">Send Message</button>
+                                <button className="btn btn-success btn-sm">SEND</button>
                             </div>
                         </div>
                     </div>
