0c0ebe67c7908fc9803ccd8440509d5ccae44064
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-07-24 15:26:41 +0500

    password hashing

--- wellnesswise-backend/models/userSchema.js
+++ wellnesswise-backend/models/userSchema.js
@@ -1,9 +1,32 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    age: Number
-})
+    email: String,
+    phone: String,
+    address: String,
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+//hashing password for privacy and safety/security.
+UserSchema.pre('save',async function(next){
+    if(this.isModified('password') || this.isNew){
+        try{
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password,salt);
+            next();
+        } catch (err){
+            next(err);
+        }
+
+    } else {
+        return next();
+    }
+});
 
 const UserModel = mongoose.model("users", UserSchema);
 export default UserModel;
