e8c143ee6d2efbfc3080fba68446ea9f1ccc449d
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-07-24 17:31:09 +0500

    Used User router

--- wellnesswise-backend/server.js
+++ wellnesswise-backend/server.js
@@ -1,21 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import UserModel from './models/userSchema.js';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/WellnessWise')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 
-app.get("/getUsers",(req,res)=>{
-    UserModel.find({}).then(function(users){
-        res.json(users)
-    });
-})
+app.use('/api/users', userRoutes);
 
 
 const port = process.env.PORT || 3000;
