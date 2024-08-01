425c58fe09fbf44a63a9391767ded3a4aaa66e78
Mehar-Aziz <meharaziz.1614@gmail.com>
2024-08-01 21:55:11 +0500

    SSL

--- wellnesswise-backend/server.js
+++ wellnesswise-backend/server.js
@@ -1,37 +1,43 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-dotenv.config('.env');
+import https from 'https';
+import fs from 'fs';
 import userRoutes from './routes/userRoutes.js';
 import resourcesRoutes from './routes/resourcesRoutes.js';
 
+// Load environment variables
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/WellnessWise')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
-
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Simple API route for testing
 app.get('/api', (req, res) => {
-    res.json({ message: 'API is working' });
-  });
-  //user route
+  res.json({ message: 'API is working' });
+});
+
+// User route
 app.use('/api/users', userRoutes);
-//
+
+// Resources route
 app.use('/api/resources', resourcesRoutes);
 
-
-
-
-
+// Define the port
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+// Read SSL certificate and key from environment variables
+const key = fs.readFileSync(process.env.SSL_KEY_PATH);
+const cert = fs.readFileSync(process.env.SSL_CERT_PATH);
+
+// Create HTTPS server
+https.createServer({ key, cert }, app).listen(port, () => {
+  console.log(`HTTPS Server is running on port ${port}`);
 });
